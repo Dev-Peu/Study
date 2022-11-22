8af5d63305140b08b8405738c704bc3d578ea580
Dev-Peu <pedrocadast@gmail.com>
2022-11-22 16:21:28 -0300

    a mesma api também formata valores.

--- vanillajs/test.js
+++ vanillajs/test.js
@@ -1,9 +1,15 @@
-const date = new Date();
-const formattedDate = new Intl.DateTimeFormat("pt-BR", {
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-}).format(date);
+// const date = new Date();
+// const formattedDate = new Intl.DateTimeFormat("pt-BR", {
+//   year: "numeric",
+//   month: "long",
+//   day: "numeric",
+// }).format(date);
 
-console.log(date);
-console.log(formattedDate);
+// console.log(date);
+// console.log(formattedDate);
+
+const price = 25321.35;
+
+const fPrice = new Intl.NumberFormat("pt-br", {}).format(price);
+
+console.log(fPrice);
